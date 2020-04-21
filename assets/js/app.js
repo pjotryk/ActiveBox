5a6e04bb3c429ad568afd93661416155f08a8738
pjotryk <pjotryk@ukr.net>
2020-04-21 22:56:43 +0300

    work_21-04-2020

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,9 @@
 $(function () {
   "use strict";
   
+  /* Fixed Header
+  ==================*/
+    
   let header = $("#header");
   let intro = $("#intro");
   let introH = intro.innerHeight();
@@ -14,13 +17,45 @@ $(function () {
     scrolPos = $(this).scrollTop();
     introH = intro.innerHeight();
     
-    if(scrolPos > introH-84) {
+    if(scrolPos > introH-header.height()) {
        header.addClass("fixed");
     } else {
        header.removeClass("fixed");
     }
   })
   
- 
+  
+  /* Smooth Scroll
+ ========================================*/
+  $("[data-scroll]").on("click", function() {
+    event.preventDefault(); /* При кліку на ссилку немає переходу за адресою.*/
+    
+    let elementID = $(this). data('scroll');
+    let elementOffset = $(elementID).offset().top;
+    
+    $("html, body").animate({
+      scrollTop:elementOffset-header.height()
+    }, 700);
+    
+    nav.removeClass("show");
+    
+  })
+  
+  
+  /* Nav Toggle
+  =========================*/
+  let nav = $("#nav");
+  let nav_toggle = $("#nav_toggle");
+  
+  nav_toggle.on("click", function(event) {
+    event.preventDefault();
+    
+    nav.toggleClass("show");
+  });
+  
+  
+  /* Reviews Slider
+  =====================*/
+    
 
 });
